c1da83e9f4741499c7afb7dc2b83b2fc1caf63f4
grosser <grosser.michael@gmail.com>
2009-01-18 17:48:43 +0100

    quadratic + fix of cubic

--- lib/07-algorithm-design/2-quadratic.rb
+++ lib/07-algorithm-design/2-quadratic.rb
@@ -4,8 +4,10 @@ require File.join(File.dirname(__FILE__),'test')
 def find_max_sum_range(range)
   max = 0
   0.upto(range.length) do |start|
+    sum=0
     start.upto(range.length-1) do |end_at|
-      max = [range[start..end_at].sum, max].max
+      sum+=range[end_at]
+      max = [sum, max].max
     end
   end
   max
--- lib/07-algorithm-design/test.rb
+++ lib/07-algorithm-design/test.rb
@@ -2,13 +2,14 @@ require 'rubygems'
 require 'activesupport'
 
 def test
-  [
-    [],
-    [1],
-    [1,2,3,4],
-    [1,4,-1,2,4,-123,12,13,34,-122,12,43]
-  ].each do |range|
+  {
+    []=>0,
+    [1]=>1,
+    [1,2,3,4]=>10,
+    [1,4,-1,2,4,-123,12,13,34,-122,12,43]=>59
+  }.each do |range,result|
+    puts
     puts range * '|'
-    puts find_max_sum_range(range)*'|'
+    puts "#{find_max_sum_range(range)} <-> #{result}"
   end
 end
