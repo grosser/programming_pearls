696bb21095c255329215240e76c04425b6cb6864
grosser <grosser.michael@gmail.com>
2009-01-19 20:14:55 +0100

    linear

---
+++ lib/07-algorithm-design/5-linear.rb
@@ -0,0 +1,14 @@
+require File.join(File.dirname(__FILE__),'test')
+
+# runtime O(n)
+def find_max_sum_range(range)
+  max = partial_max = 0
+  range.each do |x|
+    # keep adding to partial max unless it sinks below 0
+    partial_max = [partial_max+x, 0].max
+    max = [partial_max, max].max
+  end
+  max
+end
+
+test
